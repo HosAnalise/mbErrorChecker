922f45b6e2d71b6ae6e14f47592d5697b0cd3e4e
Fabiano Urquiza <urquiizaf@gmail.com>
2025-09-18 11:24:27 -0300

    feat: adiciona informação na inicialização e para erros não encontrados na tabela

--- services/update_registers/update_registers.py
+++ services/update_registers/update_registers.py
@@ -4,22 +4,35 @@ from db.MongoDbManager import MongoDbManager
 mongo_db_manager = MongoDbManager()
 data_base_manager = DatabaseManager() 
 
-def update_registers(): 
-    errors_list = mongo_db_manager.get_data()   
+def update_registers():
+    print("Iniciando atualização de dados...")
+    
+    errors_list = mongo_db_manager.get_data()
+    if not errors_list:
+        print("Nenhum erro encontrado.")
+        return
 
-    for table_name,value in mongo_db_manager.fila_tabela.items():
-        for key,value in value.items():
-            for error in errors_list:
-
-                query = f"""UPDATE 
-                                {key}
-                            SET 
-                                DATA = DATA
-                            WHERE 
-                                {value} = {error.get("code")}                             
+    for error in errors_list:
+        table_name = error.get("table_name")
+        if table_name:
+            for table_name,queue in mongo_db_manager.fila_tabela.items():
+                for key,value in queue.items():
+                    for error in errors_list:
+                        
                         
 
-                """ if error.get("table_name").lower() == table_name.lower() else ""
-                data_base_manager.execute_query(query)
+                        query = f"""UPDATE 
+                                        {key}
+                                    SET 
+                                        DATA = DATA
+                                    WHERE 
+                                        {value} = {error.get("code")}                             
+                                
+
+                        """ if error.get("table_name").lower() == table_name.lower() else ""
+
+                        print(query)
+
+                       # data_base_manager.execute_query(query)
 
 update_registers()
